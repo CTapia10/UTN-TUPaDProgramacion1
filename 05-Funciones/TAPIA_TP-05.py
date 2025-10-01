248c2d843367db6e8b23c5d4ec5fea51fcc7eeeb
CTapia10 <cristianalejandro_t@hotmail.com>
2025-10-01 14:22:43 -0300

    Programa principal + Ejercicio 1

---
+++ 05-Funciones/TAPIA_TP-05.py
@@ -0,0 +1,17 @@
+# Trabajo practico 5 - Funciones 
+# Cristian Tapia - 2025
+
+def programa_principal():
+    # Llamo a la funcion que imprime hola mundo
+    imprimir_hola_mundo()    
+
+
+# Ejercicio 1
+
+# Defino nuevo metodo para imprimir hola mundo
+def imprimir_hola_mundo():
+    print("Hola Mundo")
+
+
+# Ejecuto el programa principal
+programa_principal()
