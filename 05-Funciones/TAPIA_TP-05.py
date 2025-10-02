c837d562191f28795bf446635163770cdcc909a8
CTapia10 <cristianalejandro_t@hotmail.com>
2025-10-02 19:53:49 -0300

    Mejoro validaciones para mas robustez y documento mejor el codigo

--- 05-Funciones/TAPIA_TP-05.py
+++ 05-Funciones/TAPIA_TP-05.py
@@ -9,8 +9,8 @@ def imprimir_hola_mundo():
 
 # Ejercicio 2
 # Defino funcion para saludar al usuario
-def saludar_usuario(nombre):
-    print(f"Hola {nombre}, bienvenido/a!")  
+def saludar_usuario(nombre_valido):
+    print(f"Hola {nombre_valido}, bienvenido/a!")  
 
 
 # Ejercicio 3
@@ -25,14 +25,13 @@ def mostrar_area_y_perimetro_circulo(radio):
     import math
     # Obtengo el valor de PI
     pi = math.pi
-    radioIngresado = radio
     # Defino funcion para calcular area del circulo
     def calcular_area_circulo():
-        area = round((pi * radioIngresado ** 2),2)
+        area = round((pi * radio ** 2),2)
         print(f"El area del circulo segun el radio ingresado es: {area}")
     # Defino funcion para calcular el perimetro del circulo
     def calcular_perimetro_circulo():
-        perimetro = round((2 * pi * radioIngresado),2)
+        perimetro = round((2 * pi * radio),2)
         print(f"El perimetro del circulo segun el radio ingresado es: {perimetro}")
     # LLamo a las funciones para imprimir los resultados
     calcular_area_circulo()
@@ -61,7 +60,7 @@ def operaciones_basicas(a,b):
     suma = (a + b)
     resta = (a - b)
     multiplicacion = (a * b)
-    division = round((a / b),2)
+    division = round((a / b),2) if b != 0 else "No se puede dividir por cero"
     
     # Tupla para guardar resultados para cada operacion
     operacion_y_total = (("Suma", suma), ("Resta", resta), ("Multiplicacion", multiplicacion), ("Division", division))
@@ -89,22 +88,147 @@ def calcular_promedio(a,b,c):
     promedio = round(((a+b+c)/3),2)
     print(f"El promedio de los numeros ingresados es de: {promedio}")
 
-##############################################################################
-## TODO: Documentar bien todo, hacer mas robusto el codigo con validaciones ##
-##############################################################################
 
-# Defino el programa principal #
+
+#=============================================================================#
+#======================== Defino el programa principal =======================#
+#=============================================================================#
 def programa_principal():
-    # Llamo a la funcion que imprime hola mundo
-     imprimir_hola_mundo()    
-    # saludar_usuario(input("Ingrese su nombre: "))
-    # informacion_personal(input("Ingrese su nombre: "),input("Ingrese su apellido: "),input("Ingrese su edad: "),input("Ingrese su residencia: "))
-    # mostrar_area_y_perimetro_circulo(float(input("Ingrese el radio para calcular area y perimetro de un circulo: ")))
-    # segundos_a_horas(int(input("Ingrese la cantidad de segundos para transformar a horas: ")))
-    # tabla_multiplicar(int(input(f"Ingrese un numero entero para mostrar su tabla de multiplicar del 1 al 10: ")))
-    # operaciones_basicas(int(input("Ingrese el primer numero para realizar operaciones basicas: ")),int(input("Ingrese el segundo numero: ")))
-    # calcular_imc(int(input("Ingrese su peso en KG: ")),float(input("Ingrese su altura en metros: ")))
-    # celsius_a_fahrenheit(float(input("Ingrese la temperatura en grados Celsius que desea convertir a Fahrenheit: ")))
-    # calcular_promedio(int(input("Ingrese el primer numero: ")),int(input("Ingrese el segundo numero: ")),int(input("Ingrese el tercer numero: ")))
-# Ejecuto el programa principal #
+    
+    # Funciones para validar datos en especifico
+    def validar_edad(edad):
+        if not (1 <= edad <= 100):
+            return print(f"⚠️  Ingrese una edad entre 1 y 100.")
+        else:
+            return True
+        
+    def validar_peso(peso):
+        if not (40 <= peso <= 120):
+            return print(f"⚠️  Ingrese un peso entre 40 y 120.")
+        else:
+            return True
+        
+    def validar_altura(altura):
+        if not (1.4 <= altura <= 2.5):
+            return print(f"⚠️  Ingrese una altura entre 1.4Mts y 2.5Mts.")
+        else:
+            return True
+    
+    def msg_solicitar_datos():
+        print("Ingrese correctamente los datos que se le solicitan a continuacion")
+        
+    def validar_string(id_string):
+        while True:
+            string = input(f"{id_string}: ").strip()
+            if not string.isalpha():
+                print(f"⚠️  El campo ingresado '{id_string}' no debe estar vacio y debe estar formado por letras.")
+                continue
+            if len(string) < 2:
+                print(f"⚠️ El campo ingresado '{id_string}' es demasiado corto.")
+                continue
+            return string
+
+    def validar_num(id_num):
+        import re
+        # Expresion regular para validar si la cadena es numero decimal positivo
+        chars_permitidos = re.compile(r"^\d+(\.\d+)?$")
+        # Expresion regular para validar si la cadena es numero decimal positivo o negativo
+        
+        while True:
+            dato = input(f"{id_num}: ").strip()
+            dato = dato.replace(",", ".")
+            if id_num.lower() == "celcius": 
+                chars_permitidos_y_neg = re.compile(r"^-?\d+(\.\d+)?$") 
+                if chars_permitidos_y_neg.match(dato): 
+                    dato_num = float(dato)
+                    return dato_num
+                else:
+                    print(f"⚠️  Ingrese un número válido para el campo '{id_num}'.")
+            elif chars_permitidos.match(dato): 
+                dato_num = float(dato)
+                match id_num.lower():
+                    # Valido la edad
+                    case "edad": 
+                        if not validar_edad(dato_num):
+                            continue
+                    # Valido el peso
+                    case "peso": 
+                        if not validar_peso(dato_num):
+                            continue
+                    # Valido la altura
+                    case "altura":  
+                        if not validar_altura(dato_num):
+                            continue
+                # Si es entero exacto, devolver como int
+                if dato_num.is_integer():
+                    return int(dato_num)
+                return dato_num
+            else:
+                print(f"⚠️  Ingrese un número válido para el campo '{id_num}'.")
+
+    # Defino funcion para pedir el dato segun el nombre del dato y tipo de dato para realizar validaciones de entrada
+    def pedir_dato(nombre_dato,tipo_dato):
+        STRING = "STRING"
+        NUMERO = "NUMERO"
+        # Validaciones para verificar que el dato string ingresado es correcto
+        if tipo_dato == STRING:
+            return validar_string(nombre_dato)
+        # Validaciones para verificar que el dato numero ingresado es correcto
+        elif tipo_dato == NUMERO:
+            return validar_num(nombre_dato)
+        
+#=============================================================================#
+# Funciones de cada ejercicio, descomentar la que se quiere probar (CTRL+K+U) #
+#=============================================================================#
+    
+    #==================== Funcion Ejercicio 1 =========================#
+    # imprimir_hola_mundo()
+    
+    #==================== Funcion Ejercicio 2 =========================#
+    # msg_solicitar_datos()
+    # saludar_usuario(pedir_dato("nombre","STRING"))
+    
+    #==================== Funcion Ejercicio 3 =========================#
+    # msg_solicitar_datos()
+    # informacion_personal(pedir_dato("Nombre","STRING"),pedir_dato("Apellido","STRING"),pedir_dato("Edad","NUMERO"),pedir_dato("Residencia","STRING"))
+    
+    #==================== Funcion Ejercicio 4 =========================#
+    # print("Ingrese a continuacion el radio del circulo para calcular su area y perimetro")
+    # mostrar_area_y_perimetro_circulo(pedir_dato("Radio","NUMERO"))
+    
+    #==================== Funcion Ejercicio 5 =========================#
+    # print("Ingrese a continuacion la cantidad de segundos para conocer su equivalente en horas ")
+    # segundos_a_horas(pedir_dato("Segundos","NUMERO"))
+    
+    #==================== Funcion Ejercicio 6 =========================#
+    # print("Ingrese un numero entero para mostrar su tabla de multiplicar del 1 al 10: ")
+    # tabla_multiplicar(pedir_dato("Numero","NUMERO"))
+    
+    #==================== Funcion Ejercicio 7 =========================#
+    # print("Ingrese dos numeros para realizar operaciones basicas: ")
+    # operaciones_basicas(pedir_dato("Primer numero","NUMERO"), pedir_dato("Segundo numero","NUMERO"))
+    
+    #==================== Funcion Ejercicio 8 =========================#
+    # print("Bienvenido a la calculadora de IMC")
+    # print("Esta calculadora esta pensada para calcular peso y altura promedio de un adulto, no contempla")
+    # print("Ingrese a continuacion su peso en KG y su altura en METROS ")
+    # calcular_imc(pedir_dato("Peso","NUMERO"),pedir_dato("Altura","NUMERO"))
+    
+    #==================== Funcion Ejercicio 9 =========================#
+    # print("Ingrese la temperatura en grados Celsius que desea convertir a Fahrenheit ")
+    # celsius_a_fahrenheit(pedir_dato("Celcius","NUMERO"))
+    
+    #==================== Funcion Ejercicio 10 =========================#
+    # print("Ingrese tres numeros para calcular su promedio: ")
+    # calcular_promedio(pedir_dato("Primer numero","NUMERO"),pedir_dato("Segundo numero","NUMERO"),pedir_dato("Tercer numero","NUMERO"))
+    
+#=============================================================================#
+#==================== Fin definicion de programa principal ===================#
+#=============================================================================#
+
+
+#=============================================================================#
+#======================= Ejecuto el programa principal =======================#
+#=============================================================================#
 programa_principal()
+#=============================================================================#
