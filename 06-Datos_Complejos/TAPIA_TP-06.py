80a7a7ae05af7f240c6edae6fc3246352e4a9774
CTapia10 <cristianalejandro_t@hotmail.com>
2025-10-10 14:32:32 -0300

    Agrego ejercicios 1 2 y 3 al TP 6 Datos complejos

---
+++ 06-Datos_Complejos/TAPIA_TP-06.py
@@ -0,0 +1,33 @@
+## Trabajo practico 6 - Datos Complejos 
+## Cristian Tapia - 2025
+
+
+# Ejercicio 1
+# Inicializo el diccionario con los precios de las frutas
+precios_frutas = {'Banana': 1200, 'Anana': 2500, 'Melon': 3000, 'Uva':1450}
+
+# Agrego las nuevas frutas y sus precios
+precios_frutas['Naranja'] = 1200
+precios_frutas['Manzana'] = 1500
+precios_frutas['Pera'] = 2300
+# print(precios_frutas.keys())
+# print(precios_frutas.values())
+
+
+# Ejercicio 2
+# Actualizo el precio de las frutas
+precios_frutas['Banana'] = 1330
+precios_frutas['Manzana'] = 1700
+precios_frutas['Melon'] = 2800
+# print(precios_frutas.keys())
+# print(precios_frutas.values())
+
+
+# Ejercicio 3
+# Lista que contiene solo los nombres de las frutas
+nombres_frutas = list(precios_frutas.keys())
+# print(nombres_frutas)
+
+
+# Ejercicio 4
+#
